3aba714728c8e887a4bbe37a40b0a79d3120b008
xXmarcosX <marcos.v3567@gmail.com>
2024-06-04 18:44:13 -0300

    adicionado métodos de consumo de API

--- src/routes/Garagem.jsx
+++ src/routes/Garagem.jsx
@@ -1,41 +1,74 @@
-import CarroBMW from "../assets/bmw.jpg";
-import CarroMeca from "../assets/meca.jpg";
+import { useEffect, useState } from 'react';
 import CarroAston from "../assets/astonmartin.jpg";
 import CarroAudi from "../assets/audi.jpg";
-import CarroVolks from "../assets/volks.jpg";
+import CarroBMW from "../assets/bmw.jpg";
 import CarroHyundai from "../assets/hyundai.jpg";
-import { Link } from 'react-router-dom';
+import CarroMeca from "../assets/meca.jpg";
+import CarroVolks from "../assets/volks.jpg";
+import Car from '../components/Car';
+import Marcas from '../components/Marcas';
+import '../css/style.css';
 
 export default function Garagem() {
+
+    const [carros, setCarros] = useState([])
+
+    const marcas = [
+        {
+        marca: 'Bmw',
+        imagem: CarroBMW    
+        },
+        {
+        marca: 'Aston',
+        imagem: CarroAston    
+        },
+        {
+        marca: 'Audi',
+        imagem: CarroAudi    
+        },
+        {
+        marca: 'Hyundai',
+        imagem: CarroHyundai    
+        },
+        {
+        marca: 'Meca',
+        imagem: CarroMeca    
+        },
+        {
+        marca: 'Volks',
+        imagem: CarroVolks    
+        },
+    ]
+
+    useEffect(() => {
+        fetch('http://localhost:3000/carros')
+        .then(res => {
+            return res.json()
+        })
+        .then(data => {
+            setCarros(data)
+        })
+    }, [])
+
     return(
         <>
+           
             <section className="marcas">
-                <container className="marcas-bmw-garagem">
-                    <img src={CarroBMW} alt="carro1" />
-                    <Link to="/garagem/bmw" className="marcas-garagem-name">BMW </Link>
-                </container>                    
-                <container className="marcas-mercedes-garagem">
-                    <img src={CarroMeca} alt="carro2" />
-                    <Link to="/garagem/meca" className="marcas-mercedes-garagem-name">MERCEDES</Link>
-                </container>                    
-                <container className="marcas-astonmartin-garagem">
-                    <img src={CarroAston} alt="carro3" />
-                <Link to="/garagem/aston" className="marcas-astonmartin-garagem-name">ASTON MARTIN</Link>
-                </container>                                        
-                <container className="marcas-audi-garagem">
-                    <img src={CarroAudi} alt="carro4" />
-                <Link to="/garagem/audi" className="marcas-audi-garagem-name">AUDI</Link>
-                </container>                                        
-                <container className="marcas-volkswagen-garagem">
-                    <img src={CarroVolks} alt="carro5" />
-                <Link to="/garagem/volks" className="marcas-volkswagen-garagem-name">VOLKSWAGEN</Link>
-                </container>                                        
-                <container className="marcas-hyundai-garagem">
-                    <img src={CarroHyundai} alt="carro6" />
-                <Link to="/garagem/hyundai" className="marcas-hyundai-garagem-name">HYUNDAI</Link>
-                </container>                                        
+            {marcas.map((marca, index) => {
+                return <Marcas key={index} imagem={marca.imagem} marca={marca.marca} linkado={marca.marca}/>
+            })}
             </section>
 
+            <div className="container-carro">                  
+            {carros.map(carro => (
+                    <Car
+                        key={carro.nome}
+                        nome={carro.nome}
+                        preco={carro.preco}
+                        imagem={carro.imagem}
+                    />
+                ))}
+            </div>
         </>
     )
 }
