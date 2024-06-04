2713164dcd41594189be8b8c42882f816a97d028
xXmarcosX <marcos.v3567@gmail.com>
2024-06-04 18:41:57 -0300

    adicionado componente para marcas

---
+++ src/components/Marcas.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+
+const Marcas = ({imagem, marca, linkado}) => {
+    return(
+        <>
+        
+                <div className="marcas-bmw-garagem">
+                    <img src={imagem} alt="carro1" />
+                    <Link to={`/garagem/${linkado}`} className="marcas-garagem-name">{marca} </Link>
+                </div>                                                         
+        </>
+    )
+}
+
+export default Marcas
