57f87731712e93b21d91755b147cbeb3590aabb1
xXmarcosX <marcos.v3567@gmail.com>
2024-06-04 18:47:17 -0300

    adicionada rota para testes

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Error from './routes/Error.jsx';
-import Home from './routes/Home.jsx';
-import Garagem from './routes/Garagem.jsx';
-import Bmw from './routes/Bmw.jsx';
-import Meca from './routes/Meca.jsx';
-import Aston from './routes/Aston.jsx';
-import Hyundai from './routes/Hyundai.jsx';
-import Audi from './routes/Audi.jsx';
-import Volks from './routes/Volks.jsx';
-import Login from './routes/Login.jsx';
-import Lista from './routes/Lista.jsx';
-import Produtos from './routes/Produtos.jsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
+import Teste from './components/Teste.jsx';
+import Aston from './routes/Aston.jsx';
+import Audi from './routes/Audi.jsx';
+import Bmw from './routes/Bmw.jsx';
+import Error from './routes/Error.jsx';
+import Garagem from './routes/Garagem.jsx';
+import Home from './routes/Home.jsx';
+import Hyundai from './routes/Hyundai.jsx';
+import Lista from './routes/Lista.jsx';
+import Login from './routes/Login.jsx';
+import Meca from './routes/Meca.jsx';
+import Produtos from './routes/Produtos.jsx';
+import Volks from './routes/Volks.jsx';
 
 const router = createBrowserRouter(
   [
@@ -32,7 +33,9 @@ const router = createBrowserRouter(
         { path: '/garagem/volks', element: <Volks />},
         { path: '/login', element: <Login />},
         { path: '/lista', element: <Lista />},
-        { path:'/editarproduto/:id', element:<Produtos/>},
+        {path: '/produtos', element: <Produtos/>},
+        { path:'/editarcarro/:id', element:<Produtos/>},
+        { path:'/teste', element:<Teste/>},
       ],
     },
   ]);
