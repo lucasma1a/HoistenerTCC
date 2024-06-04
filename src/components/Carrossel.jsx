82cba3d427bc208f845c47458fae17cb359f8148
xXmarcosX <marcos.v3567@gmail.com>
2024-06-04 18:42:51 -0300

    adicionada componente carrossel

---
+++ src/components/Carrossel.jsx
@@ -0,0 +1,14 @@
+import Banner from '../assets/banner-carro.jpg'
+import '../css/style.css'
+
+const Carrossel = () => {
+    return(
+        <>
+        <div className="carrossel">
+            <img src={Banner} alt="" />
+        </div>
+        </>
+    )
+}
+
+export default Carrossel
