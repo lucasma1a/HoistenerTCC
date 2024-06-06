b1618766b41d54ebc491a343644ee18582c7ee0b
xXmarcosX <marcos.v3567@gmail.com>
2024-06-06 00:00:06 -0300

    adicionada condicional para os NavLinks

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,7 @@
 import { MdAccountCircle } from "react-icons/md";
-import {  NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Logo from '../assets/hoistener-logo.png';
-import style from '../css/Link.module.css'
+import style from '../css/Link.module.css';
 import '../css/style.css';
 
 export default function Nav()
@@ -16,16 +16,16 @@ export default function Nav()
                     <NavLink to="/" className={`nav-navbar-img`} data-test-id>
                         <img src={Logo}></img>        
                     </NavLink>
-                    <NavLink to="/" className="nav-navbar-link" data-test-id>
+                    <NavLink to="/" className={({isActive}) => `${style.navNavbarLink} ${isActive ? style.linkDestacado : ''}`} data-test-id>
                         Home
                     </NavLink>
-                    <NavLink to="/garagem" className="nav-navbar-link" data-test-id>
+                    <NavLink to="/garagem" className={({isActive}) => `${style.navNavbarLink} ${isActive ? style.linkDestacado : ''}`} data-test-id>
                         Garagem
                     </NavLink>
-                    <a href='#Contato' className='nav-navbar-link' data-test-id>
+                    <a href='#Contato' className={style.navNavbarLink} data-test-id>
                         Contato
                     </a>
-                    <NavLink to="/login" className="nav-navbar-link" data-test-id>
+                    <NavLink to="/login"  className={style.navNavbarLink} data-test-id>
                         <MdAccountCircle size={25}/>
                         {/* <HiOutlineLogout onClick={handleLogout}/> */}
                     </NavLink>
