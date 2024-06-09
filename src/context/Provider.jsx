4faba8915daaba3d27c51193bc306419644fe586
xXmarcosX <marcos.v3567@gmail.com>
2024-06-09 14:20:16 -0300

    adicionado context para o carrinho

---
+++ src/context/Provider.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import AppContext from './AppContext.jsx';
+
+export const Provider = ({ children }) => {
+    const [cart, setCart] = useState([]);
+
+    const addToCart = (car) => {
+      const existe = cart.some(item => item._id === car._id);
+      if (!existe) {
+          setCart((prevCart) => [...prevCart, car]);
+      } else {
+          alert('Este carro já está no carrinho!');
+      }
+    };
+
+    const removeFromCart = (carId) => {
+      setCart(prevCart => prevCart.filter(item => item._id !== carId));
+    };
+
+    const value = {
+        cart,
+        addToCart,
+        removeFromCart
+    };
+
+    return (
+        <AppContext.Provider value={value}>
+            {children}
+        </AppContext.Provider>
+    );
+};
+
+export default Provider;
