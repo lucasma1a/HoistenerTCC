7ea63578100c86ba1fbdae68c55a250482d828f9
Lucas <matheusmaia474@gmail.com>
2024-06-09 16:21:39 -0300

    carrossel 50%

--- src/components/Carrossel/Carrossel.jsx
+++ src/components/Carrossel/Carrossel.jsx
@@ -1,16 +1,44 @@
 import Banner from '../Carrossel/img/banner-carro.jpg'
+import Banner1 from '../Carrossel/img/banner-carro2.jpg'
+import Banner2 from '../Carrossel/img/banner-carro3.jpg'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
 import '../Carrossel/Carrossel.modules.css'
 
-const Carrossel = () => {
 
+function Carrossel(){
+    const banner = [
+        {
+            id : 1,
+            image : Banner
+        },
+        {
+            id : 2,
+            image : Banner1
+        },
+        {
+            id : 3,
+            image : Banner2
+        }
+    ]
     
     return(
         <>
-        <div className="carrossel">
-            <img src={Banner} alt="" />
+        <div className='container-carrossel'>
+            <Swiper
+                slidesPerView={1}
+                pagination={{clickable: true}}
+                navigation
+            >
+                {banner.map( (item) => (
+                <SwiperSlide key={item.id}>
+                    <img src="{item.image}" alt="Slide" className='container-carrossel-slide' />
+                </SwiperSlide>
+                ))}
+            </Swiper>
         </div>
         </>
     )
-}
 
+}
 export default Carrossel
--- src/main.jsx
+++ src/main.jsx
@@ -19,6 +19,14 @@ import Meca from './routes/Meca.jsx';
 import Produtos from './routes/Produtos.jsx';
 import Volks from './routes/Volks.jsx';
 
+import { register } from 'swiper/element/bundle'
+
+register();
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 const router = createBrowserRouter(
   [
     {
