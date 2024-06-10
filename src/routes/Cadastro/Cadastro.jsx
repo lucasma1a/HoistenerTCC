dc65763760b36495b1967c76a1d602415fd14c92
xXmarcosX <marcos.v3567@gmail.com>
2024-06-10 01:41:10 -0300

    adicionada rota para cadastro

---
+++ src/routes/Cadastro/Cadastro.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import FormCadastro from '../../components/FormCadastro/FormCadastro'
+import style from './Cadastro.module.css'
+
+const Cadastro = () => {
+  return (
+    <section className={style.container}>
+      <FormCadastro/>
+    </section>
+  )
+}
+
+export default Cadastro
