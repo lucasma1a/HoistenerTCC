3badcbdf4f9e1d82914ba410f5c189bdc2527a6c
xXmarcosX <marcos.v3567@gmail.com>
2024-06-11 23:20:05 -0300

    Adicionado infos do carro por id na tela de compras

--- src/routes/Compra/Compra.jsx
+++ src/routes/Compra/Compra.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import CompraCarro from '../../components/CompraCarro/CompraCarro'
 import style from './Compra.module.css'
  
 const Compra = () => {
    
+  const [carros, setCarros] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:4000/cars')
+    .then(response => response.json())
+    .then(dados => setCarros(dados))
+    .catch((e) => {
+      console.log(e)
+      alert('erro ao se conectar na API')
+    })
+  }, [])
+
+  const id = useParams()
+
+  const carro = carros.find((car) => {
+    return car._id === id.id
+  })
+
   return (
     <>
     <section className={style.container}>
-      <CompraCarro/>
+      {carro ? (
+         <CompraCarro car={carro}/>
+      ):
+      <h1>car not found</h1>}
+     
     </section>
     </>
   )
