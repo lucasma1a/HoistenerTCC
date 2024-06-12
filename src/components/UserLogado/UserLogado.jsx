343abdc3340a07a94714c70b4d72e188d97b34be
xXmarcosX <marcos.v3567@gmail.com>
2024-06-12 16:46:44 -0300

    adicionado componente para detalhes de usuário logado

--- src/components/UserLogado/UserLogado.jsx
+++ src/components/UserLogado/UserLogado.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import style from './UserLogado.module.css'
 
-const UserLogado = () => {
+const UserLogado = ({loggedUser}) => {
 
 
   return (
     <>
-      
+      <div className={style.container}>
+        <h1>Olá {loggedUser.name}</h1>
+
+        <div className={style.containerInfo}>
+          <p>Seu Email: {loggedUser.email}</p>
+          <p>Sua Reserva: ainda n tem</p>
+        </div>
+
+        <div className={style.containerBtns}>
+          <button>Alterar senha</button>
+          <button>Alterar E-mail</button>
+          <button>Sair</button>
+        </div>
+      </div>
     </>
   )
 }
