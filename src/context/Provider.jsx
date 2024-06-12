dbbb0c7ae14a18b7747955ed625e02a33b21f82b
xXmarcosX <marcos.v3567@gmail.com>
2024-06-12 15:17:16 -0300

    Adicionado context para usuario logado

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -3,27 +3,15 @@ import AppContext from './AppContext.jsx';
 
 export const Provider = ({ children }) => {
     const [cart, setCart] = useState([]);
-    const [user, setUser] = useState('') 
+    const [userLogado, setUserLogado] = useState({
+        name: '',
+        password: '',
+    }) 
 
-    const addToCart = (car) => {
-      const existe = cart.some(item => item._id === car._id);
-      if (!existe) {
-          setCart((prevCart) => [...prevCart, car]);
-      } else {
-          alert('Este carro já está no carrinho!');
-      }
-    };
-
-    const removeFromCart = (carId) => {
-      setCart(prevCart => prevCart.filter(item => item._id !== carId));
-    };
 
     const value = {
-        cart,
-        addToCart,
-        removeFromCart,
-        user,
-        setUser
+        userLogado,
+        setUserLogado
     };
 
     return (
