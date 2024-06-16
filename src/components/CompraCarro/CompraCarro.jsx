86c6b84e0d0e0a91b67c1b6ce11357abb0233f76
xXmarcosX <marcos.v3567@gmail.com>
2024-06-16 15:16:16 -0300

    adicionado método para consumo da api reservas e lista de reservas na tela de usuário logado

--- src/components/CompraCarro/CompraCarro.jsx
+++ src/components/CompraCarro/CompraCarro.jsx
@@ -1,138 +1,156 @@
-import React, { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import AppContext from '../../context/AppContext';
-import style from './CompraCarro.module.css';
- 
+import React, { useContext } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import AppContext from "../../context/AppContext";
+import style from "./CompraCarro.module.css";
+
 const CompraCarro = ({ car }) => {
-    
-    const navigate = useNavigate()
-    const {reserva , setReserva} = useContext(AppContext)
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const { reserva, setReserva } = useContext(AppContext);
 
-    let getId = sessionStorage.getItem('id')
-    let getToken = sessionStorage.getItem('token')
+  let getId = sessionStorage.getItem("id");
+  let getToken = sessionStorage.getItem("token");
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        if(!getId && !getToken){
-            alert('Você deve realizar o login para reservar um carro')
-            navigate('/login')
-        } else {
-        alert('Reserva efetuada com sucesso')
-        setReserva((prevReserva) => [...prevReserva, `${car.mark} ${car.model} ${formattedPrice}`])
-        console.log(reserva)
-        handleDelete(car._id)
+    if (!getId && !getToken) {
+      alert("Você deve realizar o login para reservar um carro");
+      navigate("/login");
+      return;
+    } else {
+      fetch(`http://localhost:4000/reserva/${getId}/${id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userId: getId,
+          carId: id,
+          startDate: "2017-12-16",
+          endDate: "2017-12-20",
+        })
+      }).then((r) => {
+        alert('reserva efetuada')
         navigate('/login')
-        }
+    }).catch((e) => {
+        alert('Erro: ', e)
+    })
     }
+  };
 
-    const handleDelete = (id) => {
-
-        if(!getId && !getToken){
-            return
-        } else {
-            fetch(`http://localhost:4000/car/${id}`, {
-                method: 'DELETE',
-            }).then(() => {
-                console.log('Carro da exclusão', id)
-            })
-        }
+  const handleDelete = (id) => {
+    if (!getId && !getToken) {
+      return;
+    } else {
+      fetch(`http://localhost:4000/car/${id}`, {
+        method: "DELETE",
+      }).then(() => {
+        console.log("Carro da exclusão", id);
+      });
     }
+  };
 
-    console.log('Reserva: ', reserva)
+  console.log("Reserva: ", reserva);
 
-    function formatPrice(price) {
-        return price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    }
-    
-    const formattedPrice = formatPrice(car.price);
-    return (
-        <>
-            <section className={style.container}>
-                <div className={style.containerCard}>
-                    <h1 className={style.containerTitle}>
-                        {car.mark}  {car.model}       
-                        <h2>{formattedPrice}</h2>
-                    </h1>
-                    <div className={style.containerImg}>
-                        <img src={car.image} alt={`${car.mark}, ${car.model}`} />
-                    </div>
-                    <div className={style.containerInfo}>
-                        <ul className={style.infoTop}>
-                            <li>carro</li>
-                            <li>carro</li>
-                            <li>carro</li>
-                        </ul>
-                        <ul className={style.infoBot}>
-                            <li>carro</li>
-                            <li>carro</li>
-                            <li>carro</li>
-                        </ul>
-                    </div>
- 
-                </div>
- 
-                <div className={style.form}>
- 
-                    <h3>Digite suas informações para reservar o carro</h3>
- 
-                    <form onSubmit={handleSubmit}>
-                        <label >
-                            Nome:
-                            <input type="text" 
-                            placeholder='Digite seu nome' 
-                            required={true}
-                            />
-                        </label>
- 
-                        <label >
-                            E-mail:
-                            <input type="email" 
-                            placeholder='Digite seu email'
-                            required={true}
-                            />
-                        </label>
- 
-                        <label >
-                            Telefone:
-                            <input type="text" 
-                            placeholder='Digite seu telefone' 
-                            required={true}
-                            />
-                        </label>
- 
-                        <label >
-                            CPF:
-                            <input type="number" 
-                            placeholder='Digite seu CPF' 
-                            required={true}
-                            />
-                        </label>
- 
-                        <label >
-                            Data de Nascimento:
-                            <input type="date" 
-                            placeholder='Insira sua data de nascimento' 
-                            max="2006-12-31"
-                            required={true}
-                            />
-                        </label>
- 
-                        <label >
-                            Digite seu CEP:
-                            <input type="number" 
-                            placeholder='Insira seu CEP' 
-                            required={true}
-                            />
-                        </label>
- 
-                        <button>Reservar Carro</button>
- 
-                    </form>
-                </div>
-            </section>
-        </>
-    )
-}
- 
-export default CompraCarro
+  function formatPrice(price) {
+    return price.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+  }
+
+  const formattedPrice = formatPrice(car.price);
+  return (
+    <>
+      <section className={style.container}>
+        <div className={style.containerCard}>
+          <h1 className={style.containerTitle}>
+            {car.mark} {car.model}
+            <h2>{formattedPrice}</h2>
+          </h1>
+          <div className={style.containerImg}>
+            <img src={car.image} alt={`${car.mark}, ${car.model}`} />
+          </div>
+          <div className={style.containerInfo}>
+            <ul className={style.infoTop}>
+              <li>carro</li>
+              <li>carro</li>
+              <li>carro</li>
+            </ul>
+            <ul className={style.infoBot}>
+              <li>carro</li>
+              <li>carro</li>
+              <li>carro</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className={style.form}>
+          <h3>Digite suas informações para reservar o carro</h3>
+
+          <form onSubmit={handleSubmit}>
+            <label>
+              Nome:
+              <input
+                type="text"
+                placeholder="Digite seu nome"
+                required={true}
+              />
+            </label>
+
+            <label>
+              E-mail:
+              <input
+                type="email"
+                placeholder="Digite seu email"
+                required={true}
+              />
+            </label>
+
+            <label>
+              Telefone:
+              <input
+                type="text"
+                placeholder="Digite seu telefone"
+                required={true}
+              />
+            </label>
+
+            <label>
+              CPF:
+              <input
+                type="number"
+                placeholder="Digite seu CPF"
+                required={true}
+              />
+            </label>
+
+            <label>
+              Data de Nascimento:
+              <input
+                type="date"
+                placeholder="Insira sua data de nascimento"
+                max="2006-12-31"
+                required={true}
+              />
+            </label>
+
+            <label>
+              Digite seu CEP:
+              <input
+                type="number"
+                placeholder="Insira seu CEP"
+                required={true}
+              />
+            </label>
+
+            <button>Reservar Carro</button>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default CompraCarro;
--- src/components/UserLogado/UserLogado.jsx
+++ src/components/UserLogado/UserLogado.jsx
@@ -1,12 +1,14 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import AppContext from '../../context/AppContext';
 import style from './UserLogado.module.css';
 
 const UserLogado = ({ loggedUser }) => {
+  const getId = sessionStorage.getItem('id') 
   const { id } = useParams()
   const { reserva } = useContext(AppContext);
-  console.log(reserva);
+  const [reservas, setReservas] = useState([])
+  console.log(reservas);
 
   const handleLogout = () => {
     sessionStorage.removeItem('id');
@@ -15,6 +17,19 @@ const UserLogado = ({ loggedUser }) => {
     location.reload();
   };
 
+  useEffect(() => {
+    fetch('http://localhost:4000/reservas')
+    .then((r) => r.json())
+    .then((data) => setReservas(data))
+    .catch(e => alert('erro: ', e))
+  }, [])
+
+  const foundReserva = reservas.filter((re) => {
+    return re.user._id === getId
+  })
+
+  console.log('Reserva do usuário: ', foundReserva)
+
   return (
     <div className={style.container}>
       <h1>Olá, {loggedUser.name}</h1>
@@ -22,11 +37,11 @@ const UserLogado = ({ loggedUser }) => {
       <div className={style.containerInfo}>
         <p><b>Seu Email:</b> {loggedUser.email}</p>
         <p><b>Suas Reservas:</b></p>
-        {reserva.length > 0 ? (
+        {foundReserva ? (
           <ul>
-            {reserva.map((re, index) => (
+            {foundReserva.map((re, index) => (
               <li key={index}>
-                {re}
+                {re.car.mark} {re.car.model} {re.car.plate}
               </li>
             ))}
           </ul>
