a4c8e573ae1bcc5e6a38de1bc8468647991ed8f4
xXmarcosX <marcos.v3567@gmail.com>
2024-06-15 21:56:17 -0300

    adicionado componente e método para alteração de senha

---
+++ src/components/AlterarForm/AlterarForm.jsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import style from './AlterarForm.module.css'
+
+const AlterarForm = () => {
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const [users, setUsers] = useState([])
+  const [oldPassword, setOldPassword] = useState('')
+  const [user, setUser] = useState({
+    email: '',
+    password: ''
+  })
+
+  useEffect(() => {
+    fetch('http://localhost:4000/users/')
+      .then(response => response.json())
+      .then(data => setUsers(data))
+  }, [])
+
+  useEffect(() => {
+    if (users.length > 0 && id) {
+      const foundUser = users.find(user => user._id === id)
+      if (foundUser) {
+        setUser({
+          email: foundUser.email,
+          password: ''
+        })
+      }
+    }
+  }, [users, id])
+
+  const handlePasswordChange = (e) => {
+    setUser(prevUser => ({
+      ...prevUser,
+      password: e.target.value
+    }))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetch(
+      'http://localhost:4000/login',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email: user.email, password: oldPassword })
+      }
+    ).then(async (response) => {
+      if (response.status === 200) {
+        fetch(`http://localhost:4000/user/${id}`, 
+          {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+          }
+        ).then((r) => {
+          alert('Senha alterada')
+          navigate('/login') 
+          return r.json()
+        })
+      } else {
+        return response.json().then((data) => {
+          throw new Error(data.message)
+        })
+      }
+    }).catch((e) => {
+      alert(e.message)
+    })
+  }
+
+  return (
+    <div className={style.container}>
+      <h1>Alterar Senha</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+        <b>Senha atual:</b>
+          <input
+            type="password"
+            placeholder='Digite sua senha...'
+            onChange={(e) => setOldPassword(e.target.value)}
+          />
+        </label>
+
+        <label>
+          <b>Nova senha:</b>
+          <input
+            type="password"
+            placeholder='Digite a nova senha...'
+            name="password" // Adicione esta linha
+            onChange={handlePasswordChange}
+          />
+        </label>
+
+        <button>Alterar</button>
+      </form>
+    </div>
+  )
+}
+
+export default AlterarForm
