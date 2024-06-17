43d1352460ef06b6ae7ad9250f3b7516a36ad307
xXmarcosX <marcos.v3567@gmail.com>
2024-06-16 21:47:19 -0300

    adicionada função para cancelar reserva de carros

--- src/components/UserLogado/UserLogado.jsx
+++ src/components/UserLogado/UserLogado.jsx
@@ -1,9 +1,10 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import AppContext from '../../context/AppContext';
 import style from './UserLogado.module.css';
 
 const UserLogado = ({ loggedUser }) => {
+  const navigate = useNavigate()
   const getId = sessionStorage.getItem('id') 
   const { id } = useParams()
   const { reserva } = useContext(AppContext);
@@ -28,7 +29,16 @@ const UserLogado = ({ loggedUser }) => {
     return re.user._id === getId
   })
 
-  console.log('Reserva do usuário: ', foundReserva)
+  const handleDelete =((id)=>{
+    fetch(`http://localhost:4000/reserva/${id}`,{
+      method:'delete',
+    }).then(()=>{
+      alert('Reserva Cancelada')
+      navigate('/garagem')
+    })
+   })
+
+  
 
   return (
     <div className={style.container}>
@@ -37,16 +47,19 @@ const UserLogado = ({ loggedUser }) => {
       <div className={style.containerInfo}>
         <p><b>Seu Email:</b> {loggedUser.email}</p>
         <p><b>Suas Reservas:</b></p>
-        {foundReserva ? (
+        {foundReserva.length > 0 ? (
           <ul>
             {foundReserva.map((re, index) => (
               <li key={index}>
-                {re.car.mark} {re.car.model} {re.car.plate}
+                {re.car.mark} {re.car.model} Placa: {re.car.plate} 
+                <div className={style.containerInfoBtn}>
+                  <button onClick={handleDelete.bind(this, re._id)}>Cancelar reserva</button>
+                </div>
               </li>
             ))}
           </ul>
         ) : (
-          <p>Você ainda não reservou nenhum carro</p>
+          <p>Você ainda não reservou nenhum veículo</p>
         )}
       </div>
 
--- src/routes/Garagem.jsx
+++ src/routes/Garagem.jsx
@@ -1,4 +1,3 @@
-
 import { useEffect, useState } from 'react';
 import styleBusca from '../components/Busca/Busca.module.css';
 import Car from '../components/Car/Car.jsx';
@@ -8,76 +7,65 @@ import '../css/style.css';
 
 export default function Garagem() {
 
-    const [carros, setCarros] = useState([])
+    const [carros, setCarros] = useState([]);
+    const [reservas, setReservas] = useState([]);
     const [busca, setBusca] = useState('');
-    const texto = 'Não há carros com esta marca ou modelo'
-    // const testeCarro = [
-    //     {
-    //         id: 1,
-    //         image: 'https://github.com/welltecnc.png',
-    //         price: 20,
-    //         mark: 'bmw',
-    //         model: 'i8',
-    //         color: 'black'
-    //     }
-    // ]
-
+    const texto = 'Não há carros com esta marca ou modelo';
 
     useEffect(() => {
-      try {
-         fetch("http://localhost:4000/cars")
-          .then((response) => response.json())
-          .then((dados) => setCarros(dados));
-      } catch {
-        console.log('erro ao se conectar com a API')
-      }
+        fetch("http://localhost:4000/cars")
+            .then((response) => response.json())
+            .then((dados) => setCarros(dados))
+            .catch(() => console.log('erro ao se conectar com a API de carros'));
     }, []);
 
-    const carrosFiltrados = carros.filter(carro =>
-        carro.mark.toLowerCase().includes(busca.toLowerCase()) || 
-        carro.model.toLowerCase().includes(busca.toLowerCase())
-    );
+    useEffect(() => {
+        fetch("http://localhost:4000/reservas")
+            .then((response) => response.json())
+            .then((dados) => setReservas(dados))
+            .catch(e => alert('erro: ', e));
+    }, []);
+
+    const carrosFiltrados = carros
+        .filter(carro => 
+            carro.mark.toLowerCase().includes(busca.toLowerCase()) || 
+            carro.model.toLowerCase().includes(busca.toLowerCase())
+        )
+        .filter(carro => 
+            !reservas.some(reserva => reserva.car.plate === carro.plate)
+        );
 
     const funcBusca = (e) => {
-        setBusca(e.target.value)
-    }
+        setBusca(e.target.value);
+    };
 
-  
-
-    console.log(carros)
-    return(
+    return (
         <>
-                     
             <section className="marcas">
-            <ListaMarcas/>
+                <ListaMarcas />
             </section>
 
             <div className="container-busca">
-            <input
-                className={styleBusca.busca}
-                value={busca}
-                onChange={funcBusca}
-                placeholder="Digite a marca ou modelo do carro"
-            />
+                <input
+                    className={styleBusca.busca}
+                    value={busca}
+                    onChange={funcBusca}
+                    placeholder="Digite a marca ou modelo do carro"
+                />
             </div>
-            
 
             <div className="container-carro">
-                {/* {testeCarro.map(carro => {
-                    return <Car key={carro.id} car={carro}/>
-                })}       */}
-            {carrosFiltrados.length ? (
-                carrosFiltrados.map(carro => (
-                    <Car
-                        key={carro._id}
-                        car={carro}
-                    />
-                ))
-            ) : (
-                <ErroBusca texto={texto}/>
-            )}
-
+                {carrosFiltrados.length ? (
+                    carrosFiltrados.map(carro => (
+                        <Car
+                            key={carro._id}
+                            car={carro}
+                        />
+                    ))
+                ) : (
+                    <ErroBusca texto={texto} />
+                )}
             </div>
         </>
-    )
+    );
 }
